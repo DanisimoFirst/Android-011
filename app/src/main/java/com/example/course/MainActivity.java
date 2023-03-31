da209e0f48bf5dcded45b6527d9af61fd2deea9f
DansergusMAC <103856701+DansergusMAC@users.noreply.github.com>
2023-03-31 20:55:30 +0300

    Calculation

--- app/src/main/java/com/example/course/MainActivity.java
+++ app/src/main/java/com/example/course/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.course;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,18 +11,25 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(this::ButtonClick);
+
+        findViewById(R.id.button_calc).setOnClickListener(this::buttonCalcClick);
     }
 
-    private void ButtonClick(View view){
-        TextView textView = findViewById(R.id.textView2);
-        String txt = textView.getText().toString();
-        txt += "!";
-        textView.setText(txt);
+//    private void ButtonClick(View view){
+////        TextView textView = findViewById(R.id.textView2);
+////        String txt = textView.getText().toString();
+////        txt += "!";
+////        textView.setText(txt);
+//    }
+
+
+    private void buttonCalcClick(View view){
+        Intent activityIntent = new Intent(MainActivity.this, CalcActivity.class);
+        startActivity(activityIntent);
     }
 }
